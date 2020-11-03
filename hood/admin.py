7d29c70297067dbe4d33922a0aa0e00cea19ca6f
Lourine <lourine.millicent@gmail.com>
2020-11-03 12:02:29 +0300

    Register models in admin.py

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import neighbourhood,healthservices,notifications,Business,Health,Authorities,Profile
+
+
+class HealthAdmin(admin.ModelAdmin):
+    filter_horizontal =['healthservices']
 
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Health,HealthAdmin)
+admin.site.register(Business)
+admin.site.register(healthservices)
+admin.site.register(Authorities)
+admin.site.register(Profile)
+admin.site.register(notifications)
