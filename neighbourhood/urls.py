0df6f20033361be65282750f5fa550f6fc9bf275
Lourine <lourine.millicent@gmail.com>
2020-11-03 16:38:32 +0300

    Add media and static files

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('hood.urls')),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'^logout/$',views.LogoutView.as_view(), {"next_page":'/'},name="logout"),
+    #url(r'^tinymce',include('tinymce.urls')),
 ]
