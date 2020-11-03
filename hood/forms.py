794353f08ce0609fbdff9f33200c0d57bee012fb
Lourine <lourine.millicent@gmail.com>
2020-11-03 12:09:27 +0300

    Add forms

---
+++ hood/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import notifications,Business,Profile
+
+class notificationsForm(forms.ModelForm):
+    class Meta:
+        model=notifications
+        exclude=['author','neighbourhood','post_date']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
+
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['owner','neighbourhood']
+
